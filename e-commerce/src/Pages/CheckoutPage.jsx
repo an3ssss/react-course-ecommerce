b1b39026eabf638b0042caa487a4a2177f546449
an3ssss <anesbenfardjallah921@gmail.com>
2026-07-31 07:07:12 +0100

    Spliting the header  into a component and using link

--- e-commerce/src/Pages/CheckoutPage.jsx
+++ e-commerce/src/Pages/CheckoutPage.jsx
@@ -2,31 +2,16 @@
 import '../styles/checkout/checkout-header.css';
 import '../styles/checkout/checkout.css';
 
+// Components
+import { HeaderComponent } from '../Components/HeaderComponent.jsx';
+
 export function ChackoutPage() {
     return (
         <>
 
             <title>Checkout</title>
 
-            <div className="checkout-header">
-                <div className="header-content">
-                    <div className="checkout-header-left-section">
-                    <a href="/">
-                        <img className="logo" src="images/logo.png" />
-                        <img className="mobile-logo" src="images/mobile-logo.png" />
-                    </a>
-                    </div>
-
-                    <div className="checkout-header-middle-section">
-                    Checkout (<a className="return-to-home-link"
-                        href="/">3 items</a>)
-                    </div>
-
-                    <div className="checkout-header-right-section">
-                    <img src="images/icons/checkout-lock-icon.png" />
-                    </div>
-                </div>
-            </div>
+            <HeaderComponent />
 
             <div className="checkout-page">
                 <div className="page-title">Review your order</div>
